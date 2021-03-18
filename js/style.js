ca788c09a64c8547029f3b2157b3b372b49815d7
Renato Gomes dos Santos <39703265+renatogsantos@users.noreply.github.com>
2021-03-18 09:37:54 -0300

    update
    navbar update

--- js/style.js
+++ js/style.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
-    $("button").click(function() {
-        $("nav").addClass("")
-    })
+$(window).scroll(function() {
+	if ($(document).scrollTop() > 30) {
+		$('nav').addClass('bg-navbar-scroll');
+	}      else {
+		$('nav').removeClass('bg-navbar-scroll');
+	}
+});
+
+
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $("nav").addClass("bg-navbar-scroll")
+  })
 })
