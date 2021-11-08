7d0a0668ea533541bf8b76966ade8c3de6eca78b
Renato Gomes dos Santos <39703265+renatogsantos@users.noreply.github.com>
2021-11-08 18:37:58 -0300

    update
    update

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,17 +1,17 @@
 ScrollReveal({
-  duration: 500
+  duration: 800
 })
 ScrollReveal({
   reset: false
 });
 ScrollReveal().reveal('.show-1', {
-  deley: 200
+  deley: 300
 })
 ScrollReveal().reveal('.show-2', {
-  delay: 300
+  delay: 500
 })
 ScrollReveal().reveal('.show-3', {
-  delay: 400
+  delay: 700
 })
 var slideUp = {
   distance: '50%',
